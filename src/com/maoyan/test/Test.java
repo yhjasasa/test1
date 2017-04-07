743ac74be91074d7e8f3c0c6f96d3078b2bdb1b3
殷洪建 <yinhongjian@maoyan.com>
2017-04-07 11:44:12 +0800

    拉姆达表达式初体验

---
+++ src/com/maoyan/test/Test.java
@@ -0,0 +1,16 @@
+package com.maoyan.test;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String str = "this is a test";
+        Arrays.asList(str.split(" ")).forEach(e -> System.out.println(Test.changeToUpper(e)));
+    }
+
+    private static String changeToUpper(String str){
+        return str.toUpperCase();
+    }
+}
