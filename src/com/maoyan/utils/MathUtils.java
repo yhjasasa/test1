da9ed597f7c83b6dc07d0997095803daafeb7ed0
殷洪建 <yinhongjian@maoyan.com>
2017-04-07 11:48:48 +0800

    添加目录结构并且添加文件

---
+++ src/com/maoyan/utils/MathUtils.java
@@ -0,0 +1,12 @@
+package com.maoyan.utils;
+
+/**
+ * Created by yinhongjian on 17/4/7.
+ */
+public class MathUtils {
+    public static void main(String[] args){
+        System.out.println("this is a test");
+    }
+
+
+}
